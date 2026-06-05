dd9f6ee94eaa73fad1706aff38c3e63dea377c88
mazezen <mazezen24@gmail.com>
2026-06-05 10:33:23 +0800

    style: remove CppResource

--- CppResource/01-basicProgramer/01-basicProgramer/1HelloWorld/HelloWorld.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  HelloWorld.cpp
-//  01-basicProgramer
-//
-//  Created by bz on 2026/6/3.
-//
-
-#include "HelloWorld.hpp"
--- CppResource/01-basicProgramer/01-basicProgramer/3Variable/variable.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  variable.cpp
-//  01-basicProgramer
-//
-//  Created by bz on 2026/6/3.
-//
-
-#include "variable.hpp"
